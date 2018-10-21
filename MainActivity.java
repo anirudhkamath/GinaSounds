b869fbc91d8dbd0890c2b5af1e1cabc58b55d864
Anirudh Kamath <anirudhkamath@hotmail.com>
2018-10-21 22:43:41 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -9,20 +9,24 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer m[]=new MediaPlayer[3];
-    Button one,two,three;
+    MediaPlayer m[]=new MediaPlayer[5];
+    Button one,two,three,four,five;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        one = (Button)this.findViewById(R.id.btn1);
-        two = (Button)this.findViewById(R.id.btn2);
-        three = (Button)this.findViewById(R.id.btn3);
-        for(int i=0;i<3;i++){
-            switch (i){
+        one = (Button)this.findViewById(R.id.btn11);
+        two = (Button)this.findViewById(R.id.btn12);
+        three = (Button)this.findViewById(R.id.btn13);
+        four = (Button)this.findViewById(R.id.btn21);
+        five = (Button)this.findViewById(R.id.btn22);
+        for(int i=0;i<5;i++){
+            switch(i){
                 case 0: m[0]=MediaPlayer.create(this,R.raw.wronganswer); break;
                 case 1: m[1]=MediaPlayer.create(this,R.raw.ihy); break;
                 case 2: m[2]=MediaPlayer.create(this,R.raw.lesstalkmoresolve); break;
+                case 3: m[3]=MediaPlayer.create(this,R.raw.sillysue); break;
+                case 4: m[4]=MediaPlayer.create(this,R.raw.micdrop); break;
             }
         }
         //final MediaPlayer m = MediaPlayer.create(this, R.raw.wronganswer);
@@ -53,5 +57,23 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         three.setOnClickListener(myListener);
+    }
+
+    public void fourthClick(View v){
+        OnClickListener myListener = new OnClickListener() {
+            public void onClick(View v) {
+                m[3].start();
+            }
+        };
+        four.setOnClickListener(myListener);
+    }
+
+    public void fifthClick(View v){
+        OnClickListener myListener = new OnClickListener() {
+            public void onClick(View v) {
+                m[4].start();
+            }
+        };
+        five.setOnClickListener(myListener);
     }
 }
