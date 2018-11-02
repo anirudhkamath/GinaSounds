2262c930cc13625953b2b4b0b5fa5eb1acd2c227
Anirudh Kamath <anirudhkamath@hotmail.com>
2018-11-02 19:28:00 +0530

    Add files via upload
    just realized a for loop isn't required at all.

--- MainActivity.java
+++ MainActivity.java
@@ -20,15 +20,13 @@ public class MainActivity extends AppCompatActivity {
         three = (Button)this.findViewById(R.id.btn13);
         four = (Button)this.findViewById(R.id.btn21);
         five = (Button)this.findViewById(R.id.btn22);
-        for(int i=0;i<5;i++){
-            switch(i){
-                case 0: m[0]=MediaPlayer.create(this,R.raw.wronganswer); break;
-                case 1: m[1]=MediaPlayer.create(this,R.raw.ihy); break;
-                case 2: m[2]=MediaPlayer.create(this,R.raw.lesstalkmoresolve); break;
-                case 3: m[3]=MediaPlayer.create(this,R.raw.sillysue); break;
-                case 4: m[4]=MediaPlayer.create(this,R.raw.micdrop); break;
-            }
-        }
+
+        m[0]=MediaPlayer.create(this,R.raw.wronganswer);
+        m[1]=MediaPlayer.create(this,R.raw.ihy);
+        m[2]=MediaPlayer.create(this,R.raw.lesstalkmoresolve);
+        m[3]=MediaPlayer.create(this,R.raw.sillysue);
+        m[4]=MediaPlayer.create(this,R.raw.micdrop);
+
         //final MediaPlayer m = MediaPlayer.create(this, R.raw.wronganswer);
     }
 
